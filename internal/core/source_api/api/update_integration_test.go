506bde5db3e7b20cef0a547263ca7d7ddfd2d0a7
Kostas Papageorgiou <kostas@runpanther.io>
2020-11-13 15:05:03 +0200

    Update tables only if integration adds new log types (#2006)

--- internal/core/source_api/api/update_integration_test.go
+++ internal/core/source_api/api/update_integration_test.go
@@ -19,6 +19,7 @@ package api
  */
 
 import (
+	"strconv"
 	"testing"
 	"time"
 
@@ -37,19 +38,19 @@ import (
 func TestUpdateIntegrationSettingsAwsScanType(t *testing.T) {
 	mockClient := &testutils.DynamoDBMock{}
 	dynamoClient = &ddb.DDB{Client: mockClient, TableName: "test"}
-	mockSQS := &testutils.SqsMock{}
-	sqsClient = mockSQS
 
-	evaluateIntegrationFunc = func(_ API, _ *models.CheckIntegrationInput) (string, bool, error) { return "", true, nil }
+	// Mocking health check
+	evaluateIntegrationFunc = func(_ API, _ *models.CheckIntegrationInput) (string, bool, error) {
+		return "", true, nil
+	}
 
 	getResponse := &dynamodb.GetItemOutput{Item: map[string]*dynamodb.AttributeValue{
 		"integrationId":   {S: aws.String(testIntegrationID)},
 		"integrationType": {S: aws.String(models.IntegrationTypeAWSScan)},
 	}}
-	mockClient.On("GetItem", mock.Anything).Return(getResponse, nil)
-	mockClient.On("PutItem", mock.Anything).Return(&dynamodb.PutItemOutput{}, nil)
-	mockClient.On("Scan", mock.Anything).Return(&dynamodb.ScanOutput{}, nil)
-	mockSQS.On("SendMessage", mock.Anything).Return(&sqs.SendMessageOutput{}, nil)
+	mockClient.On("GetItem", mock.Anything).Return(getResponse, nil).Once()
+	mockClient.On("PutItem", mock.Anything).Return(&dynamodb.PutItemOutput{}, nil).Once()
+	mockClient.On("Scan", mock.Anything).Return(&dynamodb.ScanOutput{}, nil).Once()
 
 	result, err := apiTest.UpdateIntegrationSettings(&models.UpdateIntegrationSettingsInput{
 		IntegrationID:    testIntegrationID,
@@ -73,20 +74,30 @@ func TestUpdateIntegrationSettingsAwsScanType(t *testing.T) {
 func TestUpdateIntegrationSettingsAwsS3Type(t *testing.T) {
 	mockClient := &testutils.DynamoDBMock{}
 	dynamoClient = &ddb.DDB{Client: mockClient, TableName: "test"}
+	mockSqsClient := &testutils.SqsMock{}
+	sqsClient = mockSqsClient
+
+	// Mocking health check
+	evaluateIntegrationFunc = func(api API, integration *models.CheckIntegrationInput) (string, bool, error) {
+		return "", true, nil
+	}
 
 	getResponse := &dynamodb.GetItemOutput{Item: map[string]*dynamodb.AttributeValue{
 		"integrationId":   {S: aws.String(testIntegrationID)},
 		"integrationType": {S: aws.String(models.IntegrationTypeAWS3)},
+		"logTypes":        {SS: aws.StringSlice([]string{"Log.TypeA"})},
 	}}
-	mockClient.On("GetItem", mock.Anything).Return(getResponse, nil)
-	mockClient.On("PutItem", mock.Anything).Return(&dynamodb.PutItemOutput{}, nil)
-	mockClient.On("Scan", mock.Anything).Return(&dynamodb.ScanOutput{}, nil)
+	mockClient.On("GetItem", mock.Anything).Return(getResponse, nil).Once()
+	mockClient.On("PutItem", mock.Anything).Return(&dynamodb.PutItemOutput{}, nil).Once()
+	mockClient.On("Scan", mock.Anything).Return(&dynamodb.ScanOutput{}, nil).Once()
+	// Send message to create new log types
+	mockSqsClient.On("SendMessage", mock.Anything).Return(&sqs.SendMessageOutput{}, nil)
 
 	result, err := apiTest.UpdateIntegrationSettings(&models.UpdateIntegrationSettingsInput{
 		S3Bucket: "test-bucket-1",
 		S3Prefix: "prefix/",
 		KmsKey:   "arn:aws:kms:us-west-2:111111111111:key/27803c7e-9fa5-4fcb-9525-ee11c953d329",
-		LogTypes: []string{"AWS.VPCFlow"},
+		LogTypes: []string{"Log.TypeB"},
 	})
 
 	expected := &models.SourceIntegration{
@@ -96,7 +107,47 @@ func TestUpdateIntegrationSettingsAwsS3Type(t *testing.T) {
 			S3Bucket:        "test-bucket-1",
 			S3Prefix:        "prefix/",
 			KmsKey:          "arn:aws:kms:us-west-2:111111111111:key/27803c7e-9fa5-4fcb-9525-ee11c953d329",
-			LogTypes:        []string{"AWS.VPCFlow"},
+			LogTypes:        []string{"Log.TypeB"},
+		},
+	}
+	assert.NoError(t, err)
+	assert.Equal(t, expected, result)
+	mockClient.AssertExpectations(t)
+	mockSqsClient.AssertExpectations(t)
+}
+
+func TestUpdateIntegrationSameLogtypes(t *testing.T) {
+	mockClient := &testutils.DynamoDBMock{}
+	dynamoClient = &ddb.DDB{Client: mockClient, TableName: "test"}
+	// Mocking health check
+	evaluateIntegrationFunc = func(api API, integration *models.CheckIntegrationInput) (string, bool, error) {
+		return "", true, nil
+	}
+
+	getResponse := &dynamodb.GetItemOutput{Item: map[string]*dynamodb.AttributeValue{
+		"integrationId":   {S: aws.String(testIntegrationID)},
+		"integrationType": {S: aws.String(models.IntegrationTypeAWS3)},
+		"logTypes":        {SS: aws.StringSlice([]string{"Log.TypeA"})},
+	}}
+	mockClient.On("GetItem", mock.Anything).Return(getResponse, nil)
+	mockClient.On("PutItem", mock.Anything).Return(&dynamodb.PutItemOutput{}, nil)
+	mockClient.On("Scan", mock.Anything).Return(&dynamodb.ScanOutput{}, nil)
+
+	result, err := apiTest.UpdateIntegrationSettings(&models.UpdateIntegrationSettingsInput{
+		S3Bucket: "test-bucket-1",
+		S3Prefix: "prefix/",
+		KmsKey:   "arn:aws:kms:us-west-2:111111111111:key/27803c7e-9fa5-4fcb-9525-ee11c953d329",
+		LogTypes: []string{"Log.TypeA"},
+	})
+
+	expected := &models.SourceIntegration{
+		SourceIntegrationMetadata: models.SourceIntegrationMetadata{
+			IntegrationID:   testIntegrationID,
+			IntegrationType: models.IntegrationTypeAWS3,
+			S3Bucket:        "test-bucket-1",
+			S3Prefix:        "prefix/",
+			KmsKey:          "arn:aws:kms:us-west-2:111111111111:key/27803c7e-9fa5-4fcb-9525-ee11c953d329",
+			LogTypes:        []string{"Log.TypeA"},
 		},
 	}
 	assert.NoError(t, err)
@@ -160,4 +211,70 @@ func TestUpdateIntegrationLastScanEnd(t *testing.T) {
 
 	assert.NoError(t, err)
 	mockClient.AssertExpectations(t)
+}
+
+func TestSlicesContainSameElements(t *testing.T) {
+	type testCase struct {
+		old, new []string
+		expect   bool
+	}
+
+	for i, tc := range []testCase{
+		{
+			old:    []string{},
+			new:    []string{},
+			expect: false,
+		},
+		{
+			old:    []string{"Log.A"},
+			new:    []string{},
+			expect: false,
+		},
+		{
+			old:    []string{},
+			new:    []string{"Log.A"},
+			expect: true,
+		},
+		{
+			old:    []string{"Log.A", "Log.B"},
+			new:    []string{"Log.A", "Log.A", "Log.B"},
+			expect: false,
+		},
+		{
+			old:    []string{"Log.A", "Log.B", "Log.A"},
+			new:    []string{"Log.A", "Log.B"},
+			expect: false,
+		},
+		{
+			old:    []string{"Log.A", "Log.B"},
+			new:    []string{"Log.A", "Log.B"},
+			expect: false,
+		},
+		{
+			old:    []string{"Log.A", "Log.B"},
+			new:    []string{"Log.B", "Log.A"},
+			expect: false,
+		},
+		{
+			old:    []string{"Log.A", "Log.B"},
+			new:    []string{"Log.A", "Log.B", "Log.C"},
+			expect: true,
+		},
+		{
+			old:    []string{"Log.A", "Log.B"},
+			new:    []string{"Log.A"},
+			expect: false,
+		},
+		{
+			old:    []string{"Log.A", "Log.B"},
+			new:    []string{"Log.C", "Log.D"},
+			expect: true,
+		},
+	} {
+		tc := tc
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			result := newLogsAdded(tc.old, tc.new)
+			assert.Equal(t, tc.expect, result)
+		})
+	}
 }
