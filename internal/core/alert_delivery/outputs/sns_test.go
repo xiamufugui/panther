3d6c7250bcb8cebc45885b291640cbd6a9af41b7
Justin Horak <41645915+horakj@users.noreply.github.com>
2020-11-25 09:49:35 -0600

    SNS Title Input Validation (#2047)
    * SNS Title Input Validation
    
    * mage gen fmt
    
    * Updated SNS Output with Additional Unit Testing
    
    * mage gen fmt
    
    * SNS Updated per change request
    
    * mage gen fmt
    
    * Added unit tests for new scenarios
    
    * reverted change in mock
    
    * Completed ocmment
    
    Co-authored-by: Justin Horak <horak@pantherlabs.local>
    Co-authored-by: panther-bot <github-service-account@runpanther.io>
    Co-authored-by: Kostas Papageorgiou <kostas@runpanther.io>

--- internal/core/alert_delivery/outputs/sns_test.go
+++ internal/core/alert_delivery/outputs/sns_test.go
@@ -23,11 +23,13 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sns"
 	"github.com/aws/aws-sdk-go/service/sns/snsiface"
 	jsoniter "github.com/json-iterator/go"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
 	alertModels "github.com/panther-labs/panther/api/lambda/delivery/models"
@@ -57,22 +59,16 @@ func TestSendSns(t *testing.T) {
 		},
 	}
 
-	analysisName := "policyName"
-	for i := 0; i < 100; i++ {
-		analysisName += "a"
-	}
-	alert.AnalysisName = aws.String(analysisName)
-
 	defaultMessage := Notification{
 		ID:          "policyId",
 		Type:        alertModels.PolicyType,
-		Name:        aws.String(analysisName),
+		Name:        aws.String("policyName"),
 		Description: aws.String("policyDescription"),
 		Severity:    "severity",
 		Runbook:     aws.String("runbook"),
 		CreatedAt:   createdAtTime,
 		Link:        "https://panther.io/policies/policyId",
-		Title:       "Policy Failure: " + analysisName,
+		Title:       "Policy Failure: policyName",
 		Tags:        []string{},
 		AlertContext: map[string]interface{}{
 			"key": "value",
@@ -84,7 +80,7 @@ func TestSendSns(t *testing.T) {
 
 	expectedSnsMessage := &snsMessage{
 		DefaultMessage: defaultSerializedMessage,
-		EmailMessage: analysisName + " failed on new resources\nFor more details please visit: https://panther.io/policies/policyId\n" +
+		EmailMessage: "policyName failed on new resources\nFor more details please visit: https://panther.io/policies/policyId\n" +
 			"Severity: severity\nRunbook: runbook\nDescription: policyDescription\nAlertContext: {\"key\":\"value\"}",
 	}
 	expectedSerializedSnsMessage, err := jsoniter.MarshalToString(expectedSnsMessage)
@@ -93,13 +89,186 @@ func TestSendSns(t *testing.T) {
 		TopicArn:         &snsOutputConfig.TopicArn,
 		Message:          &expectedSerializedSnsMessage,
 		MessageStructure: aws.String("json"),
-		Subject:          aws.String("Policy Failure: " + analysisName[0:84]),
+		Subject:          aws.String("Policy Failure: policyName"),
 	}
 
-	client.On("Publish", expectedSnsPublishInput).Return(&sns.PublishOutput{MessageId: aws.String("messageId")}, nil)
+	client.On("Publish", expectedSnsPublishInput).Return(&sns.PublishOutput{MessageId: aws.String("messageId")}, nil).Once()
 	getSnsClient = func(*session.Session, string) (snsiface.SNSAPI, error) {
 		return client, nil
 	}
+
+	result := outputClient.Sns(alert, snsOutputConfig)
+	assert.NotNil(t, result)
+	assert.Equal(t, &AlertDeliveryResponse{
+		Message:    "messageId",
+		StatusCode: 200,
+		Success:    true,
+		Permanent:  false,
+	}, result)
+	client.AssertExpectations(t)
+}
+
+func TestTruncateSnsTitle(t *testing.T) {
+	client := &testutils.SnsMock{}
+	outputClient := &OutputClient{}
+
+	snsOutputConfig := &outputModels.SnsConfig{
+		TopicArn: "arn:aws:sns:us-west-2:123456789012:test-sns-output",
+	}
+
+	var title string
+	// Generate a title that has
+	// 100 times the new line character (it should be removed)
+	// 100 times the character 'a'
+	for i := 0; i < 100; i++ {
+		title += "\na"
+	}
+	// The email subject we send should be the title but:
+	// 1. It should have no new lines
+	// 2. It should have 100 characters maximum
+	// 3. It should finish with 3 dots '...' at the end
+	expectedEmailSubject := "New Alert: "
+	for i := 0; i < 86; i++ {
+		expectedEmailSubject += "a"
+	}
+	expectedEmailSubject += "..."
+
+	createdAtTime := time.Now()
+	alert := &alertModels.Alert{
+		AlertID:             aws.String("alertID"),
+		AnalysisName:        aws.String("ruleName"),
+		Type:                alertModels.RuleType,
+		AnalysisID:          "ruleId",
+		AnalysisDescription: aws.String("ruleDescription"),
+		Severity:            "severity",
+		Runbook:             aws.String("runbook"),
+		CreatedAt:           createdAtTime,
+		Title:               &title,
+		Tags:                []string{},
+		Context: map[string]interface{}{
+			"key": "value",
+		},
+	}
+
+	defaultMessage := Notification{
+		ID:          alert.AnalysisID,
+		AlertID:     alert.AlertID,
+		Type:        alert.Type,
+		Name:        alert.AnalysisName,
+		Description: alert.AnalysisDescription,
+		Severity:    alert.Severity,
+		Runbook:     alert.Runbook,
+		CreatedAt:   alert.CreatedAt,
+		Title:       "New Alert: " + title,
+		Link:        "https://panther.io/alerts/alertID",
+		Tags:        []string{},
+		AlertContext: map[string]interface{}{
+			"key": "value",
+		},
+	}
+
+	defaultSerializedMessage, err := jsoniter.MarshalToString(defaultMessage)
+	require.NoError(t, err)
+
+	expectedSnsMessage := &snsMessage{
+		DefaultMessage: defaultSerializedMessage,
+		EmailMessage: "ruleName triggered\nFor more details please visit: https://panther.io/alerts/alertID\nSeverity: severity\n" +
+			"Runbook: runbook\nDescription: ruleDescription\nAlertContext: {\"key\":\"value\"}",
+	}
+	expectedSerializedSnsMessage, err := jsoniter.MarshalToString(expectedSnsMessage)
+	require.NoError(t, err)
+
+	expectedSnsPublishInput := &sns.PublishInput{
+		TopicArn:         &snsOutputConfig.TopicArn,
+		Message:          &expectedSerializedSnsMessage,
+		MessageStructure: aws.String("json"),
+		Subject:          aws.String(expectedEmailSubject),
+	}
+
+	client.On("Publish", expectedSnsPublishInput).Return(&sns.PublishOutput{MessageId: aws.String("messageId")}, nil).Once()
+	getSnsClient = func(*session.Session, string) (snsiface.SNSAPI, error) {
+		return client, nil
+	}
+	result := outputClient.Sns(alert, snsOutputConfig)
+	assert.NotNil(t, result)
+	assert.Equal(t, &AlertDeliveryResponse{
+		Message:    "messageId",
+		StatusCode: 200,
+		Success:    true,
+		Permanent:  false,
+	}, result)
+	client.AssertExpectations(t)
+}
+
+func TestResendEmailSubject(t *testing.T) {
+	client := &testutils.SnsMock{}
+	outputClient := &OutputClient{}
+	getSnsClient = func(*session.Session, string) (snsiface.SNSAPI, error) {
+		return client, nil
+	}
+
+	snsOutputConfig := &outputModels.SnsConfig{
+		TopicArn: "arn:aws:sns:us-west-2:123456789012:test-sns-output",
+	}
+
+	createdAtTime := time.Now()
+	alert := &alertModels.Alert{
+		AlertID:             aws.String("alertID"),
+		AnalysisName:        aws.String("ruleName"),
+		Type:                alertModels.RuleType,
+		AnalysisID:          "ruleId",
+		AnalysisDescription: aws.String("ruleDescription"),
+		Severity:            "severity",
+		Runbook:             aws.String("runbook"),
+		CreatedAt:           createdAtTime,
+		Title:               aws.String("title"),
+		Tags:                []string{},
+		Context: map[string]interface{}{
+			"key": "value",
+		},
+	}
+
+	defaultMessage := Notification{
+		ID:          alert.AnalysisID,
+		AlertID:     alert.AlertID,
+		Type:        alert.Type,
+		Name:        alert.AnalysisName,
+		Description: alert.AnalysisDescription,
+		Severity:    alert.Severity,
+		Runbook:     alert.Runbook,
+		CreatedAt:   alert.CreatedAt,
+		Title:       "New Alert: " + *alert.Title,
+		Link:        "https://panther.io/alerts/alertID",
+		Tags:        []string{},
+		AlertContext: map[string]interface{}{
+			"key": "value",
+		},
+	}
+
+	defaultSerializedMessage, err := jsoniter.MarshalToString(defaultMessage)
+	require.NoError(t, err)
+
+	expectedSnsMessage := &snsMessage{
+		DefaultMessage: defaultSerializedMessage,
+		EmailMessage: "ruleName triggered\nFor more details please visit: https://panther.io/alerts/alertID\nSeverity: severity\n" +
+			"Runbook: runbook\nDescription: ruleDescription\nAlertContext: {\"key\":\"value\"}",
+	}
+	expectedSerializedSnsMessage, err := jsoniter.MarshalToString(expectedSnsMessage)
+	require.NoError(t, err)
+
+	expectedSnsPublishInput := &sns.PublishInput{
+		TopicArn:         &snsOutputConfig.TopicArn,
+		Message:          &expectedSerializedSnsMessage,
+		MessageStructure: aws.String("json"),
+		// This is the default email title we are going to use
+		// after the SNS client has returned an 'InvalidParameter' error
+		Subject: aws.String("New Panther Alert"),
+	}
+
+	// First invocation returns "InvalidParameterError"
+	client.On("Publish", mock.Anything).Return(&sns.PublishOutput{}, awserr.New(sns.ErrCodeInvalidParameterException, "", nil)).Once()
+	// We retry second time, this time with the default title
+	client.On("Publish", expectedSnsPublishInput).Return(&sns.PublishOutput{MessageId: aws.String("messageId")}, nil)
 
 	result := outputClient.Sns(alert, snsOutputConfig)
 	assert.NotNil(t, result)
