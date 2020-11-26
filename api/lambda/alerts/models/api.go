4bf56f0437d9b8ec28ae051323a8168fa9bd9e40
Nick Angelou <nick.angelou@runpanther.io>
2020-11-25 20:14:14 -0600

    fix: remove required fields

--- api/lambda/alerts/models/api.go
+++ api/lambda/alerts/models/api.go
@@ -181,28 +181,28 @@ type ListAlertsOutput struct {
 
 // AlertSummary contains summary information for an alert
 type AlertSummary struct {
-	AlertID             string              `json:"alertId" validate:"required"`
-	Type                string              `json:"type,omitempty"`
-	RuleID              *string             `json:"ruleId" validate:"required"`
-	RuleDisplayName     *string             `json:"ruleDisplayName,omitempty"`
-	RuleVersion         *string             `json:"ruleVersion" validate:"required"`
-	DedupString         *string             `json:"dedupString,omitempty"`
-	DeliveryResponses   []*DeliveryResponse `json:"deliveryResponses" validate:"required"`
-	LogTypes            []string            `json:"logTypes" validate:"required"`
-	CreationTime        *time.Time          `json:"creationTime" validate:"required"`
-	UpdateTime          *time.Time          `json:"updateTime" validate:"required"`
-	EventsMatched       *int                `json:"eventsMatched" validate:"required"`
-	Severity            *string             `json:"severity" validate:"required"`
+	AlertID             string              `json:"alertId"`
+	Type                string              `json:"type"`
+	RuleID              *string             `json:"ruleId"`
+	RuleDisplayName     *string             `json:"ruleDisplayName"`
+	RuleVersion         *string             `json:"ruleVersion"`
+	DedupString         *string             `json:"dedupString"`
+	DeliveryResponses   []*DeliveryResponse `json:"deliveryResponses"`
+	LogTypes            []string            `json:"logTypes"`
+	CreationTime        *time.Time          `json:"creationTime"`
+	UpdateTime          *time.Time          `json:"updateTime"`
+	EventsMatched       *int                `json:"eventsMatched"`
+	Severity            *string             `json:"severity"`
 	Status              string              `json:"status,omitempty"`
-	Title               *string             `json:"title" validate:"required"`
-	LastUpdatedBy       string              `json:"lastUpdatedBy,omitempty"`
-	LastUpdatedByTime   time.Time           `json:"lastUpdatedByTime,omitempty"`
-	PolicyID            string              `json:"policyId" validate:"required"`
-	PolicyDisplayName   string              `json:"policyDisplayName" validate:"required"`
-	PolicyIntegrationID string              `json:"policyIntegrationId" validate:"required"`
-	PolicyVersion       string              `json:"policyVersion" validate:"required"`
-	ResourceTypes       []string            `json:"resourceTypes" validate:"required"`
-	ResourceID          string              `json:"resourceId" validate:"required"`
+	Title               *string             `json:"title"`
+	LastUpdatedBy       string              `json:"lastUpdatedBy"`
+	LastUpdatedByTime   time.Time           `json:"lastUpdatedByTime"`
+	PolicyID            string              `json:"policyId"`
+	PolicyDisplayName   string              `json:"policyDisplayName"`
+	PolicyIntegrationID string              `json:"policyIntegrationId"`
+	PolicyVersion       string              `json:"policyVersion"`
+	ResourceTypes       []string            `json:"resourceTypes"`
+	ResourceID          string              `json:"resourceId"`
 }
 
 // Alert contains the details of an alert
