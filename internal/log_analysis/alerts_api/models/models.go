4d5cbeac13a4d4d01bfab0725a91e53708e25f17
Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
2020-12-09 11:32:50 +0200

    fix: remove duplicated code

--- internal/log_analysis/alerts_api/models/models.go
+++ internal/log_analysis/alerts_api/models/models.go
@@ -64,16 +64,6 @@ type AlertPolicy struct {
 	ResourceID        string   `dynamodbav:"resourceId,string"` // This is the failing resource
 }
 
-// AlertPolicy represents the policy-specific fields for alerts genereated by policies
-type AlertPolicy struct {
-	PolicyID            string   `dynamodbav:"policyId,string"`
-	PolicyDisplayName   string   `dynamodbav:"policyDisplayName,string"`
-	PolicyVersion       string   `dynamodbav:"policyVersion,string"`
-	PolicyIntegrationID string   `dynamodbav:"policyIntegrationId,string"`
-	ResourceTypes       []string `dynamodbav:"resourceTypes,stringset"`
-	ResourceID          string   `dynamodbav:"resourceId,string"` // This is the failing resource
-}
-
 // Alert contains all the fields associated to the alert stored in DDB
 type Alert struct {
 	ID                  string    `dynamodbav:"id,string"`
