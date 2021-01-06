d082e314a939d8f424ad2aa82d2640066f1976c9
Patrick Hagan <56281590+glerb@users.noreply.github.com>
2021-01-05 17:05:51 -0800

    Corrections to least priv and cross-account SNS subscription (#2317)
    Important note about cross-account SNS topic subscription resources. Correction of least priv principal for cross account SNS topic policies.

--- deployments/auxiliary/terraform/panther_cloudwatch_events/variables.tf
+++ deployments/auxiliary/terraform/panther_cloudwatch_events/variables.tf
@@ -29,3 +29,11 @@ variable "master_account_id" {
   type        = string
   description = "AWS account ID of the account running the Panther backend"
 }
+
+# topic name is hardcoded in Panther infra; see master account panther-aws-event-processor
+# SQS queue access policy
+variable "sns_topic_name" {
+  type        = string
+  description = "The name of the SNS topic"
+  default     = "-PantherEventsTopic-"
+}
