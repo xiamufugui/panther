01b661f14c70e94e2dded409ecbea827f187e26c
Kostas Papageorgiou <kostas@runpanther.io>
2021-01-22 18:23:27 +0200

    Added unit tests

--- pkg/metrics/cloudwatch_test.go
+++ pkg/metrics/cloudwatch_test.go
@@ -20,23 +20,36 @@ package metrics
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNewCounter(t *testing.T) {
-	buf := bytes.NewBuffer(make([]byte, 8192))
+	buf := bytes.NewBuffer([]byte{})
 	cm := NewCWMetrics(buf)
-	counter := cm.NewCounter("test").With("dimension1", "dimensionValue1").
-		With("dimension2", "dimensionValue2")
-
-	counter.Add(1)
-	counter = cm.NewCounter("test").With("dimension1", "dimensionValue1").
-		With("dimension2", "dimensionValue2")
-	counter.Add(1)
-
+	// Stubbing the time function
+	cm.timeFunc = func() int64 {
+		return 1000
+	}
+	counter := cm.NewCounter("test")
 	assert.NoError(t, cm.Sync())
-	fmt.Println(buf.String())
+	// Assert nothing is written if there is no data present
+	assert.Equal(t, 0, buf.Len())
+
+	counter.Add(10)
+	assert.NoError(t, cm.Sync())
+	// nolint: lll
+	assert.Equal(t, `{"test":10,"_aws":{"CloudWatchMetrics":[{"Namespace":"Panther","Dimensions":[[]],"Metrics":[{"Name":"test","Unit":"Count"}]}],"Timestamp":1000}}`+"\n", buf.String())
+
+	buf.Reset()
+	assert.NoError(t, cm.Sync())
+	// Assert nothing is written this time - we have already synced
+	assert.Equal(t, 0, buf.Len())
+
+	counter.With("dimension1", "dimensionValue1").Add(1)
+	counter.With("dimension2", "dimensionValue2").Add(2)
+	assert.NoError(t, cm.Sync())
+	// nolint: lll
+	assert.Equal(t, `{"test":1,"dimension1":"dimensionValue1","test":2,"dimension2":"dimensionValue2","_aws":{"CloudWatchMetrics":[{"Namespace":"Panther","Dimensions":[["dimension1"],["dimension2"]],"Metrics":[{"Name":"test","Unit":"Count"},{"Name":"test","Unit":"Count"}]}],"Timestamp":1000}}`+"\n", buf.String())
 }
