7bc363139831c2905ecb3f07c895e5c6d340b2c8
Alex <alex.a.mylonas@gmail.com>
2021-01-22 15:57:28 +0200

    fix: minor update

--- web/src/components/BulletedValueList/BulletedValueList.tsx
+++ web/src/components/BulletedValueList/BulletedValueList.tsx
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 import React from 'react';
 import { Flex } from 'pouncejs';
 import LimitItemDisplay from 'Components/LimitItemDisplay/LimitItemDisplay';
